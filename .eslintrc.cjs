0150d704e1eadfe91275e14153935f45a2fe1d63
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-01-25 23:11:19 -0500

    Install dependencies, project setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,12 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, vitest: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -17,4 +18,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
