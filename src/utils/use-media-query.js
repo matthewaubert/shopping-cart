3e07913cbfbf19a9b62573dd1e862de0ecc32b8a
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-02-06 18:45:20 -0500

    Build useMediaQuery custom hook

---
+++ src/utils/use-media-query.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+/**
+ * custom React hook: check if window matches given media query
+ * @param {string} mediaQuery - e.g. '(max-width: 875px)'
+ * @returns {boolean}
+ */
+export default function useMediaQuery(mediaQuery) {
+  const [matchesMediaQuery, setMatchesMediaQuery] = useState(
+    window.matchMedia(mediaQuery).matches,
+  );
+
+  // on window change: check if matches media query && set state to bool value
+  useEffect(() => {
+    const handleMatchMediaChange = (e) => setMatchesMediaQuery(e.matches);
+
+    window
+      .matchMedia(mediaQuery)
+      .addEventListener('change', handleMatchMediaChange);
+
+    return () => {
+      window
+        .matchMedia(mediaQuery)
+        .removeEventListener('change', handleMatchMediaChange);
+    };
+  }, [mediaQuery]);
+
+  return matchesMediaQuery;
+}
