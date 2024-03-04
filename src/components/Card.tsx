14338c3c7f773f5f19bc5b78e66586ce31386dbb
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-03-04 17:57:37 -0500

    Refactor Card, Rating to add types

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,15 @@
-import PropTypes from 'prop-types';
 import Rating from './Rating';
 import { formatPrice } from '../utils/util';
+import { CartItem } from '../types';
 import '../styles/Card.css';
 
+interface CardProps {
+  data: CartItem;
+  children?: JSX.Element;
+}
+
 // React component to display each product
-function Card({ data, children }) {
+export default function Card({ data, children }: CardProps) {
   return (
     <div className="card">
       <img src={data.image} alt=""></img>
@@ -12,26 +17,8 @@ function Card({ data, children }) {
         {data.title}
       </h2>
       <div className="price">{formatPrice(data.price)}</div>
-      <Rating rating={data.rating} />
+      <Rating rate={data.rating.rate} count={data.rating.count} />
       {children}
     </div>
   );
 }
-
-Card.propTypes = {
-  data: PropTypes.shape({
-    category: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    id: PropTypes.number.isRequired,
-    image: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    rating: PropTypes.shape({
-      count: PropTypes.number.isRequired,
-      rate: PropTypes.number.isRequired,
-    }),
-    title: PropTypes.string.isRequired,
-  }).isRequired,
-  children: PropTypes.element,
-};
-
-export default Card;
--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,43 +1,45 @@
-import PropTypes from 'prop-types';
 import { roundHalf } from '../utils/util';
 
-// React component that displays given rating as x/5 stars and number of votes
-function Rating({ rating }) {
-  const accessibleRating = `Rated ${rating.rate} out of 5 stars`;
-  const accessibleTotal = `${rating.count} total votes`;
+interface RatingProps {
+  rate: number;
+  count: number;
+}
+
+// React component that displays given X/5 rating as stars and count of votes
+export default function Rating({ rate, count }: RatingProps) {
+  const accessibleRating = `Rated ${rate} out of 5 stars`;
+  const accessibleTotal = `${count} total votes`;
 
   return (
     <div className="rating">
-      <Stars rating={rating.rate} accessibleRating={accessibleRating} />
+      <Stars rate={rate} accessibleRating={accessibleRating} />
       <span title={accessibleTotal} aria-label={accessibleTotal}>
-        ({rating.count})
+        ({count})
       </span>
     </div>
   );
 }
 
-Rating.propTypes = {
-  rating: PropTypes.shape({
-    count: PropTypes.number.isRequired,
-    rate: PropTypes.number.isRequired,
-  }).isRequired,
-};
+const fullStar = (
+  <path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
+);
+const halfStar = (
+  <path d="M12,15.4V6.1L13.71,10.13L18.09,10.5L14.77,13.39L15.76,17.67M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z" />
+);
+const emptyStar = (
+  <path d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z" />
+);
 
-function Stars({ rating, accessibleRating }) {
-  const fullStar = (
-    <path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
-  );
-  const halfStar = (
-    <path d="M12,15.4V6.1L13.71,10.13L18.09,10.5L14.77,13.39L15.76,17.67M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z" />
-  );
-  const emptyStar = (
-    <path d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z" />
-  );
+interface StarsProps {
+  rate: number;
+  accessibleRating: string;
+}
 
-  const starsArr = [];
+function Stars({ rate, accessibleRating }: StarsProps) {
+  const starsArr: JSX.Element[] = [];
 
   // round rating to nearest half integer
-  const roundedRating = roundHalf(rating);
+  const roundedRating = roundHalf(rate);
   // add numFullStars full stars to starsArr
   const numFullStars = Math.floor(roundedRating);
   for (let i = 0; i < numFullStars; i++) starsArr.push(fullStar);
@@ -61,10 +63,3 @@ function Stars({ rating, accessibleRating }) {
     </span>
   );
 }
-
-Stars.propTypes = {
-  rating: PropTypes.number.isRequired,
-  accessibleRating: PropTypes.string.isRequired,
-};
-
-export default Rating;
